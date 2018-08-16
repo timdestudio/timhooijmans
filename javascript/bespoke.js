197431a796cd1844bcea400b58f66e971b588d03
timdestudio <timhooijmans@gmail.com>
2018-08-16 07:10:56 +0200

    home

--- javascript/bespoke.js
+++ javascript/bespoke.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 
-	
+	var myElement = document.getElementById('l-images-wrapper');
 
+	var hammertime = new Hammer((myElement));
+	hammertime.on('pan', function(ev) {
+
+		console.log(ev);
+		
+	});
   
 });//document ready
 
