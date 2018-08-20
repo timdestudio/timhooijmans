d4c4e70ba7e98660d1dc972394dd1f7d33e45c56
timdestudio <timhooijmans@gmail.com>
2018-08-20 13:16:03 +0200

    titles scroll version 2

--- javascript/bespoke.js
+++ javascript/bespoke.js
@@ -13,7 +13,7 @@ var Manager = {
 
 	init:function(){
 
-		//put the image clusters and titles in arrays
+		//put the image clusters and titles DOM references in arrays
 		Manager.imageClusters = $('.images-cluster');
 		Manager.projectTitles = $('.project-title-wrapper');
 
@@ -21,7 +21,7 @@ var Manager = {
 		console.log('image clusters: ' + Manager.imageClusters.length);
 		console.log('project Titles: ' + Manager.projectTitles.length);
 
-		//set project titles to match the cluster positions
+		//position the titles according to the image clusters
 		Manager.positionTitles();
 
 		//on horizontal scroll
@@ -35,11 +35,155 @@ var Manager = {
 	},//init end
 	positionTitles:function(){
 
-		//for the amount of image clusters
+		//for loop the amount of image clusters
 		for(i=0;i<Manager.imageClusters.length;i++){
 
-			//get left offset of current cluster
+			//store left offset of current cluster
 			var clusterLeft = $(Manager.imageClusters[i]).offset().left;
+
+			//store width of current cluster and title
+			var clusterWidth = $(Manager.imageClusters[i]).width();
+			var titleWidth = $(Manager.projectTitles[i]).outerWidth();
+
+			//store viewport width
+			var vw = window.innerWidth;
+
+			//calculate and store tail of cluster and title
+			var clusterTail = clusterWidth - vw;
+			var titleTail = titleWidth - vw;
+
+			//calculate ratio for tail travel
+			var tailRatio = titleTail / clusterTail;
+
+			//calculate tail position
+			var tailPos = clusterWidth - vw;
+
+
+			//if current cluster has a negative offset
+			if( clusterLeft < 0 ){
+
+				if(Math.abs(clusterLeft) < tailPos){
+
+					//set new left position calculated with ratio
+					var newLeft = tailRatio * (clusterLeft);
+
+				}else{
+
+					var newLeft = clusterLeft + (clusterWidth - titleWidth);
+				
+				}
+
+			//if current cluster doesn't have a negative offset
+			}else{
+
+				//set new left position to cluster position
+				var newLeft = clusterLeft;
+
+			}//if statement end
+
+			//adjust the title to the cluster offset
+			$(Manager.projectTitles[i]).css('left',newLeft);
+
+		}//for loop end
+
+	}//position titles end
+
+};//Manager
+
+
+
+/*---------------------------------------
+
+VERSION 1
+
+positionTitles:function(){
+
+	//for the amount of image clusters
+	for(i=0;i<Manager.imageClusters.length;i++){
+
+		//get left offset of current cluster
+		var clusterLeft = $(Manager.imageClusters[i]).offset().left;
+
+		//if current cluster has a negative offset
+		if(clusterLeft<0){
+
+			var margin = 50;
+
+			//calculate and set ratio
+			var ratio = $(Manager.projectTitles[i]).width() / ($(Manager.imageClusters[i]).width() - margin);
+
+			//log ratio
+			console.log('ratio: '+ratio);
+
+			//set new left position calculated with ratio
+			var newLeft = ratio * (clusterLeft);
+
+		//if current cluster doesn't have a negative offset
+		}else{
+
+			//set new left position to cluster position
+			var newLeft = clusterLeft;
+
+		}//if statement end
+
+		//adjust the title to the cluster offset
+		$(Manager.projectTitles[i]).css('left',newLeft);
+
+		//log the positions
+		console.log('cluster ' + i + ' postion: ' + $(Manager.imageClusters[i]).offset().left);
+		console.log('title '+i+' postion: ' + $(Manager.projectTitles[i]).offset().left);
+
+	}//for loop end
+
+}//position titles end
+
+
+
+---------------------------------------*/
+
+
+
+
+
+/*--------------------------------------
+
+VERSION 2
+
+	positionTitles:function(){
+
+		//for loop the amount of image clusters
+		for(i=0;i<Manager.imageClusters.length;i++){
+
+			//store left offset of current cluster
+			var clusterLeft = $(Manager.imageClusters[i]).offset().left;
+
+			//store width of current cluster and title
+			var clusterWidth = $(Manager.imageClusters[i]).width();
+			var titleWidth = $(Manager.projectTitles[i]).outerWidth();
+
+			//store viewport width
+			var vw = window.innerWidth;
+
+			//calculate and store tail of cluster and title
+			var clusterTail = clusterWidth - vw;
+			var titleTail = titleWidth - vw;
+
+			//calculate ratio for tail travel
+			var tailRatio = titleTail / clusterTail;
+
+			//log tail
+			console.log('****** \n tail travel ratio \n ******' + tailRatio);
+
+			//log above info
+			console.log('viewport width: ' + vw);
+			console.log('cluster width: ' + clusterWidth);
+			console.log('title width: ' + titleWidth);
+			console.log('cluster tail: ' + clusterTail);
+			console.log('title tail: ' + titleTail);
+
+			//calculate "tail" offset between cluster and title
+			var tailOffset = clusterWidth - titleWidth;
+
 
 			//if current cluster has a negative offset
 			if(clusterLeft<0){
@@ -53,7 +197,7 @@ var Manager = {
 				console.log('ratio: '+ratio);
 
 				//set new left position calculated with ratio
-				var newLeft = ratio * (clusterLeft);
+				var newLeft = tailRatio * (clusterLeft);
 
 			//if current cluster doesn't have a negative offset
 			}else{
@@ -74,4 +218,4 @@ var Manager = {
 
 	}//position titles end
 
-};//Manager
+---------------------------------------*/
